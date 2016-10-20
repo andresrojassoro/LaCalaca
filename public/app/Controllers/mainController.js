50067109abd5d83da8e3ec48739e0ca469e93a36
INTERTECINTL\arojas <andres.rojas@intertecintl.com>
2016-10-19 18:11:52 -0600

    Angular Structure

---
+++ public/app/Controllers/mainController.js
@@ -0,0 +1,30 @@
+﻿app.controller('MainCtrl', ['$scope',
+function ($scope, posts) {
+    $scope.title = '';
+    //$scope.posts = [];
+    $scope.posts = posts.posts;
+    $scope.addPost = function () {
+        if ($scope.title === '') {
+            return;
+        }
+        $scope.posts.push({
+            title: $scope.title,
+            url: 'https://cdn.vectorstock.com/i/composite/19,59/sugar-skull-vector-1541959.jpg',
+            upvotes: 0,
+            comments: [
+              { author: 'Joe', body: 'Cool post!', upvotes: 0 },
+              { author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0 }
+            ]
+        });
+        //clear the values
+        $scope.title = '';
+        $scope.url = '';
+    };
+
+    $scope.upvote = function (post) {
+        posts.upvote(post);
+    };
+    $scope.downvote = function (post) {
+        posts.downvote(post);
+    };
+}]);
