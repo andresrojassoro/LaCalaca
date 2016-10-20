25f11f673ad1637e16ec6d80fcdeee53266d93c0
INTERTECINTL\arojas <andres.rojas@intertecintl.com>
2016-10-20 09:57:10 -0600

    MongoDB added

---
+++ node/public/javascripts/Controllers/postController.js
@@ -0,0 +1,31 @@
+﻿app.controller('PostsCtrl', ['$scope', 'posts', 'post',
+function ($scope, postsh) {
+    $scope.post = posts.posts[$stateParams.id];
+
+    $scope.addComment = function () {
+        if ($scope.body === '') { return; }
+        $scope.post.comments.push({
+            body: $scope.body,
+            author: 'user',
+            upvotes: 0
+        });
+        $scope.body = '';
+    };
+    $scope.upvote = function (comment) {
+        posts.upvoteComment(post, comment);
+    };
+
+    $scope.downvote = function (comment) {
+        posts.downvoteComment(post, comment);
+    };
+
+}]);
+
+//Post Factory
+app.factory('posts', ['$http',
+function ($http) {
+    var o = {
+        posts: []
+    };
+    return o;
+}]);
